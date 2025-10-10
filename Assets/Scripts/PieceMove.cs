10a1f8ee1fdf0b7eb8e3b1a140bdbc804d64604a
yagopereiracarvalho <yagopereiracarvalho72@gmail.com>
2025-10-10 15:33:54 -0300

    Auteraçoens do PiceMove

--- Assets/Scripts/PieceMove.cs
+++ Assets/Scripts/PieceMove.cs
@@ -1,14 +1,21 @@
 using System.Diagnostics;
+using NUnit.Framework.Constraints;
 using Unity.VisualScripting;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class PieceMove : MonoBehaviour
 {
+    #region Variaves 
     float fall;
     [SerializeField] float timeCount;
     float cout;
-    [SerializeField] float timeCountFlip;
+    //[SerializeField] float timeCountFlip;
     float countFlip;
+    #endregion
+    //[SerializeField] float timeCountDown;
+    float countDown;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -22,19 +29,29 @@ public class PieceMove : MonoBehaviour
         Flip();
         Piecefall();
 
-        if (cout > 0)
-        {
-            cout -= Time.deltaTime;
-        }
-        if (countFlip > 0)
-        {
-            countFlip -= Time.deltaTime;
-        }
+        cout = UpdateTimer(cout);
+        countDown = UpdateTimer(countDown);
+        countFlip = UpdateTimer(countFlip);
+
+    //    // if (cout > 0)
+        //     {
+        //         cout -= Time.deltaTime;
+        //     }
+        //     if (countFlip > 0)
+        //     {
+        //         countFlip -= Time.deltaTime;
+        //     }
+
+        //     if (countDown > 0)
+        //     {
+        //         countDown -= Time.deltaTime;
+        //     }
 
     }
     void Move()
     {
         float horizontal = InputManager.GetMovementInpt().x;
+        float vertical = InputManager.GetMovementInpt().y;
         if (horizontal != 0 && cout <= 0)
         {
             cout = timeCount;
@@ -43,12 +60,18 @@ public class PieceMove : MonoBehaviour
         if (horizontal == 0)
             cout = 0;
 
+        if (vertical == -1 && countDown <= 0)
+        {
+            transform.position += new Vector3(0, -1, 0);
+            countDown = timeCount;
+        }
+
     }
     void Flip()
     {
         if (InputManager.GetMovementInput() && countFlip <= 0)
         {
-            countFlip = timeCountFlip;
+            countFlip = timeCount;
             transform.Rotate(0, 0, 90);
         }
     }
@@ -59,6 +82,14 @@ public class PieceMove : MonoBehaviour
             transform.position += new Vector3(0, -1, 0);
             fall = Time.time;
         }
+    }
+    float UpdateTimer(float timer)
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        return timer;
     }
 
 }
